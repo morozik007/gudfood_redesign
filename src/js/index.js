1036b870c141cb46556bb763196ebb93d6400131
Andrey Morozov <am@norse.digital>
2019-10-01 14:06:35 +0300

    forms

--- src/js/index.js
+++ src/js/index.js
@@ -81,87 +81,96 @@ $(function() {
         }
     });
 
-    //$('#phone').inputmask({"mask": "+38(999)999-99-99"});
-    // $("#first-try-form").validate();
-    // $("#second-try-form").validate();
-    $("#popup-try-form").validate(
-        // rules:{
-        //     phoneUS:
-        //     {
-        //     required:true,
-        //     phoneUS:true
-        //     }
-        // }
-    );
-
-    // $.validator.addMethod( "phoneUS", function( phone_number, element ) {
-    //     return phone_number.match(/^\+[1-9]{2}-[0-9]{3}-[0-9]{3}-[0-9]{4}$/)
-    // }, "Please specify a valid phone number" );
-
-    // $('#first-try-form input').on('submit', function(e) {
-    //     e.preventDefault();
-    //     // if ($(this).validate().checkForm()) {                   
-    //     //     $(this).submit();
-    //     // } else {
-    //     //     console.log('fail');
-    //     // }
-    // })
-
-    // $('#first-try-form input').bind('keyup blur click', function () { 
-    //     if ($('#first-try-form').validate().checkForm()) {                   
-    //         $('#first-try-submit').removeClass('button_disabled').prop('disabled', false); 
-    //     } else {
-    //         $('#first-try-submit').addClass('button_disabled').prop('disabled', true);
-    //     }
-    // });
-
-    // $('#second-try-form input').bind('keyup blur click', function () { 
-    //     if ($('#second-try-form').validate().checkForm()) {                   
-    //         $('#second-try-submit').removeClass('button_disabled').prop('disabled', false); 
-    //     } else {
-    //         $('#second-try-submit').addClass('button_disabled').prop('disabled', true);
-    //     }
-    // });
-
-    // $('#popup-try-form input').bind('keyup blur click', function () { 
-    //     if ($('#popup-try-form').validate().checkForm()) {                   
-    //         $('#popup-try-submit').removeClass('button_disabled').prop('disabled', false); 
-    //     } else {
-    //         $('#popup-try-submit').addClass('button_disabled').prop('disabled', true);
-    //     }
-    // });
-
-    $('.sendForm').on('submit', function(e){
-        e.preventDefault();
-        
-        if ( $(this).validate().checkForm() ) {
-            $.ajax({
-                url: $(this).attr('action'),
-                data: $(this).serialize(),
-                dataType: 'json',
-                type: $(this).attr('method'),
-                success: function(data) {
-                    if(data.status == 'ok') {
-                        console.log('submitted');
-                        $.fancybox.close();
-                        $.fancybox.open({ 
-                            src: '#thanks',
-                            baseClass: 'popup-transparent'
-                        });
-                    } else {
-                        console.log('fail');
-                    }
-                },
-                error: function() {
-                    console.log('error');
-                    $.fancybox.open('<div class="message"><h2>Oops!</h2><p>Something went wrong</p></div>');
-                }
-            });
-        }
-        else {
-            console.log('fail');
+    
+    $('#send-form input').bind('keyup blur click', function () { 
+        if ($('#send-form').validate().checkForm()) {                   
+            $('#send-submit').removeClass('button_disabled').prop('disabled', false); 
+        } else {
+            $('#send-submit').addClass('button_disabled').prop('disabled', true);
         }
     });
+
+    $('#popup-send-form input').bind('keyup blur click', function () { 
+        if ($('#popup-send-form').validate().checkForm()) {                   
+            $('#popup-send-submit').removeClass('button_disabled').prop('disabled', false); 
+        } else {
+            $('#popup-send-submit').addClass('button_disabled').prop('disabled', true);
+        }
+    });
+
+    $('.fb').on('click', function(e) {
+        fbq('track', 'Lead')
+    });
+
+    $('#send-form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            type: $(this).attr('method'),
+            success: function(data) {
+                if(data.status == 'ok') {
+                    //console.log('form 2 ok');
+                    window.location.replace("/success.php");
+                    //$('#form2-ok').show();
+                    //fbq('track', 'Lead');
+                } 
+            },
+            
+        });
+    });
+
+    $('#popup-send-form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            type: $(this).attr('method'),
+            success: function(data) {
+                if(data.status == 'ok') {
+                    //console.log('form 2 ok');
+                    window.location.replace("/success.php");
+                    //$('#form2-ok').show();
+                    //fbq('track', 'Lead');
+                } 
+            },
+            
+        });
+    });
+
+    // $('.sendForm').on('submit', function(e){
+    //     e.preventDefault();
+        
+    //     if ( $(this).validate().checkForm() ) {
+    //         $.ajax({
+    //             url: $(this).attr('action'),
+    //             data: $(this).serialize(),
+    //             dataType: 'json',
+    //             type: $(this).attr('method'),
+    //             success: function(data) {
+    //                 if(data.status == 'ok') {
+    //                     console.log('submitted');
+    //                     $.fancybox.close();
+    //                     $.fancybox.open({ 
+    //                         src: '#thanks',
+    //                         baseClass: 'popup-transparent'
+    //                     });
+    //                 } else {
+    //                     console.log('fail');
+    //                 }
+    //             },
+    //             error: function() {
+    //                 console.log('error');
+    //                 $.fancybox.open('<div class="message"><h2>Oops!</h2><p>Something went wrong</p></div>');
+    //             }
+    //         });
+    //     }
+    //     else {
+    //         console.log('fail');
+    //     }
+    // });
 
     //    
 });
